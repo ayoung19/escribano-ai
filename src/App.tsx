d336e9a070248e9b6cde4fbe32aae7f4052ca52b
ayoung <zeusandninja@gmail.com>
2022-02-02 05:09:06 -0800

    Change subcategories color

--- src/App.tsx
+++ src/App.tsx
@@ -166,7 +166,7 @@ export const App: FC = () => {
                               setIsFlyoutVisible(false);
                             },
                             size: "s",
-                            color: "primary",
+                            color: "text",
                           }))}
                         />
                       </EuiAccordion>
